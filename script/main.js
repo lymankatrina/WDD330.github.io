9badf0cf5c13b12a33a9afe5ed080550a45f4ed4
Katrina L Lyman <77120233+lymankatrina@users.noreply.github.com>
2022-10-25 10:41:37 -0600

    Add files via upload

---
+++ script/main.js
@@ -0,0 +1,5 @@
+document.getElementById("currentyear").textContent =
+	new Date().toLocaleDateString("en-US", options);
+
+let updatedate = Date.parse(document.lastModified);
+document.getElementById("updatedate").textContent = updatedate;
